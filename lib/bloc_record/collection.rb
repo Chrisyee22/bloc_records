bcbb2dea514f52b0ee4bf9cb1fe45ba9efb217dd
Chris <chris.anthony.yee@gmail.com>
2018-10-04 20:49:57 -0700

    update

---
+++ lib/bloc_record/collection.rb
@@ -0,0 +1,26 @@
+module BlocRecord
+  class Collection < Array
+
+    def update_all(updates)
+      ids = self.map(&:id)
+
+      self.any? ? self.first.class.update(ids, updates) : false
+    end
+
+    def take(num=1)
+      if self.any?
+        self[0, num]
+      else
+        false
+      end
+    end
+
+    def where(hash)
+      self.select {|obj| obj.attributes[hash.keys.first] == hash.values.first}
+    end
+
+    def not
+      self.select {|obj| obj.attributes[hash.keys.first] != hash.values.first}
+    end
+   end
+ end
--- lib/bloc_record/persistence.rb
+++ lib/bloc_record/persistence.rb
@@ -28,6 +28,14 @@ module Persistence
      true
    end
 
+   def update_attribute(attribute, value)
+     self.class.update(self.id, { attribute => value })
+   end
+
+   def update_attribute(updates)
+     self.class.update(self.id, updates)
+   end
+
   module ClassMethods
     def create(attrs)
       attrs = BlocRecord::Utility.convert_keys(attrs)
@@ -42,6 +50,46 @@ module Persistence
       data = Hash[attributes.zip attrs.values]
       data["id"] = connection.execute("SELECT last_insert_rowid();")[0][0]
       new(data)
+    end
+
+    def update(ids, updates)
+     if updates.is_a?(Array)
+         0.upto (ids.length-1) do |i|
+           update(ids[i], updates[i])
+         end
+
+       true
+      else
+          updates = BlocRecord::Utility.convert_keys(updates)
+          updates.delete "id"
+          updates_array = updates.map { |key, value | "#{key}=#{BlocRecord::Utility.sql_strings(value)}"}
+
+          if ids.class == Fixnum
+            where_clause = "WHERE id = #{ids};"
+          elsif ids.class == Array
+            where_clause = ids.empty? ? "," : "WHERE id IN (#{ids.join(",")});"
+          else
+            where_clause = ";"
+          end
+
+          connection.execute <<-SQL
+            UPDATE #{table}
+            SET #{updates_array * ","} #{where_clause}
+          SQL
+
+          true
+      end
+    end
+
+    def method_missing(method, *args, &block)
+      if method.include? "update"
+        m = method[method.index("_")+1..-1]
+      end
+      update_attribute(m, arg[0])
+    end
+
+    def update_all(updates)
+      update(nil, updates)
     end
   end
 end
